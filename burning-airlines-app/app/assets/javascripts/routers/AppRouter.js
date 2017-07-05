4694808e8fa2d34425e2f63bfbca8bdb4c246401
Zinh AL-Sweedy <zin_tafe@yahoo.com.au>
2017-07-05 17:54:12 +1000

    last flight show

--- burning-airlines-app/app/assets/javascripts/routers/AppRouter.js
+++ burning-airlines-app/app/assets/javascripts/routers/AppRouter.js
@@ -2,13 +2,22 @@ var app = app || {};
 
 app.AppRouter = Backbone.Router.extend({
   routes: {
-    '': 'search'
+    '': 'search',
+    'flights/:id': 'showFlight'
   },
   search: function(){
-    console.log('Navigated to default route initializeFlights ()');
+    console.log('Navigated to default route search');
     var appView = new app.appView({
       collection: app.flights
     });
     appView.render();
+  },
+  showFlight: function( id ){
+    console.log('flight id',id);
+    var flight = app.flights.get( id );
+    var fv = new app.FlightView({
+      model: flight
+  });
+    fv.render();
   }
 });
--- burning-airlines-app/app/assets/javascripts/views/FlightView.js
+++ burning-airlines-app/app/assets/javascripts/views/FlightView.js
@@ -1,25 +1,14 @@
 var app = app || {};
 
-
-
-
 app.FlightView = Backbone.View.extend({
-  tagName: "tr",
+  el: "#reservationsTable",
   render: function(){
 
-    console.log('flight view running')
+    var rawTemplate = $("#ReservationViewTemplate").html();
+    var template = _.template( rawTemplate );
 
-    // debugger;
-    console.log(this);
-    var date = this.model.get("date");
-    this.$el.html(date);
-    var flight = this.model.get("flight_no");
-    this.$el.html(flight)
-    var from = this.model.get("origin");
-    this.$el.html(from);
-    var to = this.model.get("destination");
-    this.$el.html(to);
-    this.$el.prependTo( "#flights"); // prepend add to top  append to the end
-    console.log(('el', this.$el));
+    var markup = template( this.model.attributes );
+
+    this.$el.html( markup );
   }
 });
