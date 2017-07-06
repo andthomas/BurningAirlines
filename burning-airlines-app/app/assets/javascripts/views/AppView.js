94d63f15bf526ff7f63cd7e555c6ff7284032033
Andrew Brown <andrewtcb@gmail.com>
2017-07-07 08:47:41 +1000

    added

--- burning-airlines-app/app/assets/javascripts/views/AppView.js
+++ burning-airlines-app/app/assets/javascripts/views/AppView.js
@@ -2,21 +2,40 @@ var app = app || {};
 app.appView = Backbone.View.extend({
   el: "#app",
 
+  events: {
+    "click button": "searchFlight",
+  },
+
   render: function(){
+    var rawTemplate = $("#AppViewTemplate").html();
+    this.$el.html(rawTemplate);
 
-    var template = $('#AppViewTemplate').html();
-    this.$el.html( template );
+  },
 
-    this.collection.each(function(flight){
+  searchFlight: function(){
+
+    var from = this.$el.find("#tb1").val();
+    var to = this.$el.find("#tb2").val();
+    console.log(from);
+    console.log(to);
+
+    var journeys = app.flights.models;
+
+    // console.log(journeys)
+    var result = _.filter(journeys, function(flight){
+      return flight.get("origin") === from && flight.get("destination") === to;
+      console.log(flight);
+    });
+
+    _.each(result, function(flight){
       var flv = new app.FlightListView({
         model: flight
       });
       flv.render();
     });
 
-    console.log(app.flights)
-
-    console.log('AppView rendered')
 
   }
+
+
 });
--- burning-airlines-app/app/assets/javascripts/views/FlightView.js
+++ burning-airlines-app/app/assets/javascripts/views/FlightView.js
@@ -2,53 +2,58 @@ var app = app || {};
 
 app.FlightView = Backbone.View.extend({
   el: "#app",
+
   render: function(){
 
-    debugger;
     console.log( this.model.attributes )
-    console.log('FlightView');
 
     var rawTemplate = $("#ReservationViewTemplate").html();
     var template = _.template( rawTemplate );
-
     var markup = template( this.model.attributes );
 
-    console.log( this.model.attributes )
+    var totalRows = this.model.attributes.airplane.rows;
+    var totalCols = this.model.attributes.airplane.columns;
+    var rows = _.range(1, totalRows+1);
+    var cols = _.range(1, totalCols+1);
 
+    var seatingPlan = $("<table class='seatingplan'/>").css({
+            'border':"1px solid black",
+            "max-width" :"800px"
+    });
+
+    _.each(rows, function(r){
+      var $row = $("<tr>");
+      $row.appendTo(seatingPlan);
+      _.each(cols, function(c){
+        var $col = $("<td class='seat'>").css({
+                  'border':"1px solid black",
+                  "width" : "40px",
+                  "height" : "50px"
+                });
+        $col.appendTo($row);
+      });
+    });
     this.$el.html( markup );
+    this.$el.append( seatingPlan );
+    //
+    // $(document).on('click', 'td', function(){
+    // var selectedSeat = app.flights.fetch();
+    // _.each(selectedSeat.responseJSON, function(num){
+    //    _.each(num.reservations, function(reservation){
+    //          console.log( "seat num:", reservation.seat_col);
+    //      });
+    //   });
+    // });
+  },
 
-    rows = this.model.attributes.airplane.rows;
-    cols = this.model.attributes.airplane.columns;
+  events: {
+    "click td": "seatSelect"
+  },
 
-    // _.each(rows, function(r){
-    //   $('<tr>').appendTo('#table')
-    // })
-    // _.each(cols, function(c){
-    //   $('<td>').appendTo('#table')
-    // })
-
-  //   mytable = $('<table></table>').attr({ id: "basicTable" });
-	// // var rows = new Number($("#rowcount").val());
-	// // var cols = new Number($("#columncount").val());
-  // //
-  //
-  //
-  // table = $('<table></table>').attr({ id: "basicTable" });
-  // var rows = new Number($("#rowcount").val());
-	// var cols = new Number($("#columncount").val());
-  //
-  //
-  // 	var tr = [];
-  // 	for (var i = 0; i < rows; i++) {
-  // 		var row = $('<tr></tr>').attr({ class: ["class1"].join(' ') }).appendTo(table);
-  // 		for (var j = 0; j < cols; j++) {
-  // 			$('<td></td>').text("text1").appendTo(row);
-  // 		}
-  //
-  // 	}
-  //
-
-
-
+  seatSelect: function(){
+    console.log('cliqued')
   }
+
+
+
 });
