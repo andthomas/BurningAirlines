9c0ed1c510a0ec1217c1fc4f17ae28f86195c3fd
Zinh AL-Sweedy <zin_tafe@yahoo.com.au>
2017-07-06 17:30:02 +1000

    fixed flight show page

--- burning-airlines-app/app/assets/javascripts/views/FlightView.js
+++ burning-airlines-app/app/assets/javascripts/views/FlightView.js
@@ -4,6 +4,9 @@ var app = app || {};
 app.FlightView = Backbone.View.extend({
   el: "#app",
   render: function(){
+
+    debugger;
+    console.log( this.model.attributes )
     console.log('FlightView');
     // debugger;
     var rawTemplate = $("#ReservationViewTemplate").html();
@@ -15,8 +18,38 @@ app.FlightView = Backbone.View.extend({
 
     this.$el.html( markup );
 
-    console.log( this.model.attributes.airplane.rows )
-    console.log( this.model.attributes.airplane.columns )
+    rows = this.model.attributes.airplane.rows;
+    cols = this.model.attributes.airplane.columns;
+
+    // _.each(rows, function(r){
+    //   $('<tr>').appendTo('#table')
+    // })
+    // _.each(cols, function(c){
+    //   $('<td>').appendTo('#table')
+    // })
+
+  //   mytable = $('<table></table>').attr({ id: "basicTable" });
+	// // var rows = new Number($("#rowcount").val());
+	// // var cols = new Number($("#columncount").val());
+  // //
+  //
+  //
+  // table = $('<table></table>').attr({ id: "basicTable" });
+  // var rows = new Number($("#rowcount").val());
+	// var cols = new Number($("#columncount").val());
+  //
+  //
+  // 	var tr = [];
+  // 	for (var i = 0; i < rows; i++) {
+  // 		var row = $('<tr></tr>').attr({ class: ["class1"].join(' ') }).appendTo(table);
+  // 		for (var j = 0; j < cols; j++) {
+  // 			$('<td></td>').text("text1").appendTo(row);
+  // 		}
+  //
+  // 	}
+  //
+
+
 
   }
 });
